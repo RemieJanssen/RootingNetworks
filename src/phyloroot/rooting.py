5154b8fb012dec8454bcbbbc56c5eaddd8b5e147
Remie Janssen <remiejanssen92@gmail.com>
2026-07-04 18:46:10 +0200

    refactor expansion of reduced network

--- src/phyloroot/rooting.py
+++ src/phyloroot/rooting.py
@@ -8,6 +8,21 @@ from phyloroot.class_checkers import (
 from phyloroot.constrained_orientation import constrained_orientation_binary
 from phyloroot.chain_reduction import reduce_chains
 
+def get_leaf_adjacent_to_node(network, node):
+    """Returns the leaf node adjacent to a node in the network, if it exists.
+
+    Args:
+        network (nx.Graph): The network
+        node (int): The node in the network
+
+    Returns:
+        int or None: The leaf node adjacent to the node, or None if no such node exists.
+    """
+    for nb in network.neighbors(node):
+        if network.degree(nb) == 1:
+            return nb
+    return None
+
 
 def leaf_edges(blob):
     """Returns a list of leaf edges of the blob in the order (neighbor,leaf)"""
@@ -68,6 +83,106 @@ def c_orientation_exponential(network, class_checker=is_network):
     return rootings
 
 
+def expand_reduced_rootings_for_short_side(reduced_network, reduced_rootings, reduced_side):
+    """Expands the rootings of a reduced network to the original network for sides of length <= ell+2
+
+    Args:
+        reduced_network (nx.Graph): The reduced network
+        reduced_rootings (dict(tuple(int))): A dict with all valid C-root-edges of the reduced network as keys, and a tuple of
+            reticulation nodes of a valid C-orientation with this root edge of `reduced_network`.
+        reduced_side (list(int)): The list of nodes in the reduced side
+
+    Returns:
+        dict(tuple(int)): A dict with all valid C-root-edges on the side of the original network corresponding to `reduced_side` 
+        as keys, and a tuple of reticulation nodes of a valid C-orientation with this root edge of `network`.
+    """
+
+    all_side_edges = [(reduced_side[i], reduced_side[i + 1]) for i in range(len(reduced_side) - 1)]
+    all_side_leaf_edges = [(node, nb) for node in reduced_side[1:-1] for nb in reduced_network.neighbors(node) if reduced_network.degree(nb) == 1]
+    rootings = dict()
+    for edge in all_side_edges + all_side_leaf_edges:
+        rootingAtEdge = reduced_rootings.get(edge)
+        if not rootingAtEdge:
+            rootingAtEdge = reduced_rootings.get((edge[1], edge[0]))
+        if rootingAtEdge:
+            rootings[edge] = rootingAtEdge
+    return rootings
+
+def expand_reduced_rootings_for_long_side(network, reduced_network, reduced_rootings, reduced_side, side, ell):
+    """Expands the rootings of a reduced network to the original network for sides of length > ell+2
+
+    Uses the fact that the rootings of the network are determined by the rootings at the leaf edges of the reduced side.
+
+    Args:
+        network (nx.Graph): The original network
+        reduced_network (nx.Graph): The reduced network
+        reduced_rootings (dict(tuple(int))): A dict with all valid C-root-edges of the reduced network as keys, and a tuple of
+            reticulation nodes of a valid C-orientation with this root edge of `reduced_network`.
+        reduced_side (list(int)): The list of nodes in the reduced side
+        side (list(int)): The list of nodes in the original side
+        ell (int): The length to which chains are reduced.
+    
+    Returns:
+        dict(tuple(int)): A dict with all valid C-root-edges on the side of the original network corresponding to `reduced_side` 
+        as keys, and a tuple of reticulation nodes of a valid C-orientation with this root edge of `network`.
+    """
+
+    rootings = dict()
+    # Infer rootings from leaf edge rootability.
+    n = len(side) - 2 # number of leaves on the side, called n in the paper
+    for index, current in enumerate(reduced_side[1:-1]):
+        i = index + 1
+        # Find the leaf edge attached to current node
+        leaf = get_leaf_adjacent_to_node(reduced_network, current)
+        # Check if the leaf edge (current,leaf) is a root-edge in the reduced network
+        rooting_at_edge = reduced_rootings.get((current, leaf))
+        if not rooting_at_edge:
+            rooting_at_edge = reduced_rootings.get((leaf, current))
+        if not rooting_at_edge:
+            continue
+
+        # If the network is rootable at the leaf edge, infer rootings of the original network
+        # First the leaf edges on the side
+        for j in range(i, n - (ell - i) + 1):
+            # Find the leaf edge at position j
+            side_node = side[j]
+            leafEdge = (side_node, get_leaf_adjacent_to_node(network, side_node))
+            # Now we extend to an orientation of the original network
+            # be careful with inferring orientation: there can be a reticulation on the root side.
+            # In that case, shift the reticulation to the relative position to the root as in the reduced network
+            newRooting = []
+            for reticulation in rooting_at_edge:
+                if reticulation in reduced_side[1:-1]:
+                    # Here, we have found a reticulation node reticNode on the reduced side containing the root.
+                    # We find its position on the reduced side, and give it the same relative position to the root on the original network
+                    reducedIndex = reduced_side.index(reticulation)
+                    # Current position of the root is j
+                    # In the reduced side, the root is at position i, and the retic at position reducedIndex, a difference of reducedIndex-i.
+                    newRooting += [side[j + reducedIndex - i]]
+                else:
+                    # If the reticulation node is not on the root side, we can take this node as the reticulation (by placing the leaves back where they were removed)
+                    newRooting += [reticulation]
+            rootings[leafEdge] = tuple(newRooting)
+        # Now the internal edges of the side
+        for j in range(i - 1, n - (ell - i) + 1):
+            # Do something similar as for the leaf edges, to find the right reticulation node on the root side
+            newRooting = []
+            for reticulation in rooting_at_edge:
+                if reticulation in reduced_side[1:-1]:
+                    reducedIndex = reduced_side.index(reticulation)
+                    # In the reduced side, the root is at position i, and the retic at position reducedIndex, a difference of reducedIndex-i.
+                    # To make up for the fact that a new leaf is introduced between positions j and j+1 when we root at an internal edge
+                    # we correct the new position as follows:
+                    relativePosition = reducedIndex - i
+                    if reducedIndex < i:
+                        relativePosition += 1
+                    newRooting += [side[j + relativePosition]]
+                else:
+                    # If the reticulation node is not on the root side, we can take this node as the reticulation (by placing the leaves back where they were removed)
+                    newRooting += [reticulation]
+            rootings[(side[j], side[j + 1])] = tuple(newRooting)
+    return rootings
+
 def c_orientation_fpt_reticulation_number(network, ell, class_checker=is_network):
     """Solves C-orientation for the given network and class
     This uses the FPT-time algorithm with the reticulation number as parameter
@@ -85,7 +200,7 @@ def c_orientation_fpt_reticulation_number(network, ell, class_checker=is_network
             A function that determines whether a network is in a certain class
 
     Returns:
-        dict(tuple(int): A dict with all valid C-root-edges as keys, and a tuple of
+        dict(tuple(int)): A dict with all valid C-root-edges as keys, and a tuple of
         reticulation nodes of a valid C-orientation with this root edge of `network`.
     """
 
@@ -93,90 +208,13 @@ def c_orientation_fpt_reticulation_number(network, ell, class_checker=is_network
         # In this case, the chain reduction would result in parallel edges, so keep the chain length 3 in this case.
         ell = 3
     reduced_network, sidesDict = reduce_chains(network, ell)
-    redRootings = c_orientation_exponential(reduced_network, class_checker)
+    reduced_rootings = c_orientation_exponential(reduced_network, class_checker)
     rootings = dict()
-    for redSide, side in sidesDict.items():
+    for reduced_side, side in sidesDict.items():
         if len(side) <= ell + 2:
-            # Go through all leaf-edges and internal edges and copy the rootability to the network
-            previous = redSide[0]
-            for current in redSide[1:]:
-                # Check if there is a leaf edge attached to current node
-                for nb in reduced_network.neighbors(current):
-                    nbDegree = reduced_network.degree(nb)
-                    if nbDegree == 1:
-                        # Check the leaf edge (current,nb)
-                        rootingAtEdge = redRootings.get((current, nb))
-                        if not rootingAtEdge:
-                            rootingAtEdge = redRootings.get((nb, current))
-                        if rootingAtEdge:
-                            rootings[(current, nb)] = rootingAtEdge
-                # Check the internal edge (previous,current)
-                rootingAtEdge = redRootings.get((previous, current))
-                if not rootingAtEdge:
-                    rootingAtEdge = redRootings.get((current, previous))
-                if rootingAtEdge:
-                    rootings[(previous, current)] = rootingAtEdge
-                previous = current
+            rootings = {**rootings, **expand_reduced_rootings_for_short_side(reduced_network, reduced_rootings, reduced_side)}
         else:
-            # Infer rootings from leaf edge rootability.
-            n = len(side) - 2
-            for index, current in enumerate(redSide[1:-1]):
-                i = index + 1
-                # Find the leaf edge attached to current node
-                for nb in reduced_network.neighbors(current):
-                    nbDegree = reduced_network.degree(nb)
-                    if nbDegree == 1:
-                        leaf = nb
-                # Check the leaf edge (current,leaf)
-                rootingAtEdge = redRootings.get((current, leaf))
-                if not rootingAtEdge:
-                    rootingAtEdge = redRootings.get((leaf, current))
-                if rootingAtEdge:
-                    # If rootable at leaf edge, infer rootings of the original network
-                    # First the leaf edges on the side
-                    for j in range(i, n - (ell - i) + 1):
-                        # Find the leaf edge at position j
-                        sideNode = side[j]
-                        for nb in network.neighbors(sideNode):
-                            if (
-                                network.degree(nb) == 1
-                            ):  # If this holds, we have found the corresponding leaf, so the leaf edge is (sideNode,nb)
-                                leafEdge = (sideNode, nb)
-                                break
-                        # Now we extend to an orientation of the original network
-                        # be careful with inferring orientation: there can be a reticulation on the root side.
-                        # In that case, shift the reticulation to the relative position to the root as in the reduced network
-                        newRooting = []
-                        for reticNode in rootingAtEdge:
-                            if reticNode in redSide[1:-1]:
-                                # Here, we have found a reticulation node reticNode on the reduced side containing the root.
-                                # We find its position on the reduced side, and give it the same relative position to the root on the original network
-                                reducedIndex = redSide.index(reticNode)
-                                # Current position of the root is j
-                                # In the reduced side, the root is at position i, and the retic at position reducedIndex, a difference of reducedIndex-i.
-                                newRooting += [side[j + reducedIndex - i]]
-                            else:
-                                # If the reticulation node is not on the root side, we can take this node as the reticulation (by placing the leaves back where they were removed)
-                                newRooting += [reticNode]
-                        rootings[leafEdge] = tuple(newRooting)
-                    # Now the internal edges of the side
-                    for j in range(i - 1, n - (ell - i) + 1):
-                        # Do something similar as for the leaf edges, to find the right reticulation node on the root side
-                        newRooting = []
-                        for reticNode in rootingAtEdge:
-                            if reticNode in redSide[1:-1]:
-                                reducedIndex = redSide.index(reticNode)
-                                # In the reduced side, the root is at position i, and the retic at position reducedIndex, a difference of reducedIndex-i.
-                                # To make up for the fact that a new leaf is introduced between positions j and j+1 when we root at an internal edge
-                                # we correct the new position as follows:
-                                relativePosition = reducedIndex - i
-                                if reducedIndex < i:
-                                    relativePosition += 1
-                                newRooting += [side[j + relativePosition]]
-                            else:
-                                # If the reticulation node is not on the root side, we can take this node as the reticulation (by placing the leaves back where they were removed)
-                                newRooting += [reticNode]
-                        rootings[(side[j], side[j + 1])] = tuple(newRooting)
+            rootings = {**rootings, **expand_reduced_rootings_for_long_side(network, reduced_network, reduced_rootings, reduced_side, side, ell)}
     return rootings
 
 
