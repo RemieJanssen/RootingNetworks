e792925e9f19017e027e304bfb91f098ff3f9f53
Remie Janssen <remiejanssen92@gmail.com>
2026-07-05 10:33:07 +0200

    connstruct full orientation in separate function

--- src/phyloroot/rooting.py
+++ src/phyloroot/rooting.py
@@ -254,6 +254,69 @@ def get_blobs_and_leaf_edges(network):
     return blobs_and_leaf_edges
 
 
+def construct_full_orientation(blobs_and_leaf_edges, blob_orientations, root_edge):
+    """Constructs a full orientation of the network from the orientations of the blobs and a given root edge.
+
+    Starts at the blob containing the root edge, and moves away from the root to orient all other blobs.
+
+    Args:
+        blobs_and_leaf_edges (list(tuple(nx.Graph, list(tuple(int))))): a list of tuples, where each tuple contains a blob (as a subgraph) and a list of leaf edges (as tuples of nodes with the leaf as the second element of the pair) for that blob.
+        blob_orientations (list(dict(tuple(int), list(int)))): a list of dictionaries, where each dictionary contains the orientations of a blob (as keys) and the corresponding reticulation nodes (as values).
+        root_edge (tuple(int)): The root edge of the network.
+    
+    Returns:
+        list(int) or bool: A list of reticulation nodes of a valid C-orientation with the given root edge of the network if it exists, and False otherwise.
+    """
+    # Find a blob containing the root edge
+    for i, (blob, blob_leaf_edges) in enumerate(blobs_and_leaf_edges):
+        if blob.has_edge(*root_edge):
+            break
+
+    reticulations = []
+    edges_to_continue_at = False
+    # check if the blob containing the potential root edge can be rooted at this edge, and if so, continue to root the other blobs
+    if len(blob) == 2:
+        edges_to_continue_at = set([root_edge, (root_edge[1], root_edge[0])])
+    elif root_edge in blob_orientations[i]:
+        reticulations += blob_orientations[i][root_edge]
+        edges_to_continue_at = blob_leaf_edges
+    elif (root_edge[1], root_edge[0]) in blob_orientations[i]:
+        reticulations += blob_orientations[i][(root_edge[1], root_edge[0])]
+        edges_to_continue_at = blob_leaf_edges
+    else:
+        # If the blob containing the potential root edge cannot be rooted at this edge, continue to the next edge
+        return False
+
+    # Continue to root all other blobs, by moving away from the blob with the root.
+    # edges_to_continue_at keeps a list of edges along which we still have to move away from the root
+    while edges_to_continue_at:
+        edge = edges_to_continue_at.pop()
+        # find the blob this edge points to
+        for i, (blob, blob_leaf_edges) in enumerate(blobs_and_leaf_edges):
+            if edge[1] not in blob:
+                continue
+            # Continue at trivial biconnected components with an endpoint edge[1] (but not edge[0], to prevent cycling in the algorithm)
+            if len(blob) == 2 and edge[0] not in blob:
+                other_node = False
+                for v in blob:
+                    if v != edge[1]:
+                        other_node = v
+                edges_to_continue_at.add((edge[1], other_node))
+            # Continue at blobs that contain edge[1] in the interior (so the degree of edge[1] in the blob is not 1)
+            if len(blob) > 2 and blob.degree(edge[1]) != 1:
+                edges_to_continue_at |= blob_leaf_edges - set(
+                    [(edge[1], edge[0])]
+                )
+                if edge in blob_orientations[i]:
+                    reticulations += blob_orientations[i][edge]
+                else:
+                    reticulations += blob_orientations[i][
+                        (edge[1], edge[0])
+                    ]
+                break
+    return reticulations
+
+        
 def c_orientation_fpt_level(network, ell, ClassChecker=is_network):
     """Solves C-orientation for the given network and class
     This uses the FPT-time algorithm with the level as parameter
@@ -283,7 +346,7 @@ def c_orientation_fpt_level(network, ell, ClassChecker=is_network):
     for blob, blob_leaf_edges in blobs_and_leaf_edges:
         if len(blob) <= 2:
             # For trivial biconnected components, add a trivial list to the blob orientations, 
-            # so that indices still match between blobOrientations, and blobs
+            # so that indices still match between blob_orientations, and blobs
             blob_orientations += [[]]
             continue
         # Find all rootings of the blob
@@ -312,61 +375,18 @@ def c_orientation_fpt_level(network, ell, ClassChecker=is_network):
     root_component = is_tree(T_CN)
     if not type(root_component) == int:
         return False
+    root_component_nodes = T_CN.nodes(data=True)[root_component]["members"]
 
     # Go through all edges to find all root-edges
     rootings = dict()
-    root_component_nodes = T_CN.nodes(data=True)[root_component]["members"]
     for root_edge in network.edges:
-        # Check if the edge is in the rootComponent
+        # Check if the edge is in the root_component
         if not (root_edge[0] in root_component_nodes and root_edge[1] in root_component_nodes):
             continue
-        # Find a blob containing the root edge
-        for i, (blob, blob_leaf_edges) in enumerate(blobs_and_leaf_edges):
-            if blob.has_edge(*root_edge):
-                break
-
-        reticulations = []
-        edges_to_continue_at = False
-        # check if the blob containing the potential root edge can be rooted at this edge, and if so, continue to root the other blobs
-        if len(blob) == 2:
-            edges_to_continue_at = set([root_edge, (root_edge[1], root_edge[0])])
-        elif root_edge in blob_orientations[i]:
-            reticulations += blob_orientations[i][root_edge]
-            edges_to_continue_at = blob_leaf_edges
-        elif (root_edge[1], root_edge[0]) in blob_orientations[i]:
-            reticulations += blob_orientations[i][(root_edge[1], root_edge[0])]
-            edges_to_continue_at = blob_leaf_edges
-        else:
-            # If the blob containing the potential root edge cannot be rooted at this edge, continue to the next edge
+        extended_rooting = construct_full_orientation(
+            blobs_and_leaf_edges, blob_orientations, root_edge
+        )
+        if not extended_rooting:
             continue
-
-        # Continue to root all other blobs, by moving away from the blob with the root.
-        # edges_to_continue_at keeps a list of edges along which we still have to move away from the root
-        while edges_to_continue_at:
-            edge = edges_to_continue_at.pop()
-            # find the blob this edge points to
-            for i, (blob, blob_leaf_edges) in enumerate(blobs_and_leaf_edges):
-                if edge[1] not in blob:
-                    continue
-                # Continue at trivial biconnected components with an endpoint edge[1] (but not edge[0], to prevent cycling in the algorithm)
-                if len(blob) == 2 and edge[0] not in blob:
-                    otherNode = False
-                    for v in blob:
-                        if v != edge[1]:
-                            otherNode = v
-                    edges_to_continue_at.add((edge[1], otherNode))
-                    break
-                # Continue at blobs that contain edge[1] in the interior (so the degree of edge[1] in the blob is not 1)
-                if len(blob) > 2 and blob.degree(edge[1]) != 1:
-                    edges_to_continue_at |= blob_leaf_edges - set(
-                        [(edge[1], edge[0])]
-                    )
-                    if edge in blob_orientations[i]:
-                        reticulations += blob_orientations[i][edge]
-                    else:
-                        reticulations += blob_orientations[i][
-                            (edge[1], edge[0])
-                        ]
-                    break
-        rootings[root_edge] = reticulations
+        rootings[root_edge] = extended_rooting
     return rootings
